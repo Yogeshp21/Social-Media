531e1c827c9510dcf2a75142f0274fa1f844d5dc
Yogesh Patade <yogeshpatade2019@gmail.com>
2024-04-04 09:58:46 +0530

    first commite

--- Server/connection.js
+++ Server/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 module.exports = async () => {
-    const dbUri = "mongodb+srv://yogesh:yogesh@cluster0.kq3d3hr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    const dbUri = process.env.envREACT_APP_MONGODB_URI
     try {
         await mongoose.connect(dbUri)
         console.log("MongoDB is connected")
